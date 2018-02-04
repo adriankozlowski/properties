c3f9e77290635d892db57e3013a9d61a8f9919e2
Adrian Kozłowski <adrian.kozlowski@outlook.com>
2018-02-04 10:11:04 +0100

    komentarze

--- src/main/java/pl/sda/Main.java
+++ src/main/java/pl/sda/Main.java
@@ -6,20 +6,7 @@ import java.util.Properties;
 public class Main {
 
     public static void main(String[] args) {
-        try {
-            Properties properties = new Properties();
-            properties.load(new FileInputStream("ustawienia.properties"));
-            String imieWlasciela = (String) properties.get("imie_wlasciela");
-
-            properties.setProperty("property1","wartosc");
-            properties.setProperty("property2","wartosc2");
-            properties.setProperty("property3","wartosc3");
-            properties.setProperty("property4","wartosc4");
-            properties.store(new FileOutputStream("ustawienia.properties"),null);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        String imie_wlasciciela = Register.getInstance().get("imie_wlasciciela");
+        System.out.println(imie_wlasciciela);
     }
 }
--- src/main/java/pl/sda/Register.java
+++ src/main/java/pl/sda/Register.java
@@ -13,40 +13,79 @@ public class Register {
     private Properties properties;
 
 
+    /**
+     * Tworzę instancję klasy Register
+     */
     private Register() {
+        //zapewniam sobie istnieje obiektu properties
         properties = new Properties();
+        //tworzę uchwyt do pliku properties
         File file = new File(FILENAME);
+        //sprawdzam czy plik istnieje
         if(file.exists()){
+            //jezeli istnieje, to odczytuję go moją metodą do odczytu pliku.
             load();
         } else{
+            //jezeli nie istnieje, to zapisujuę pusty plik wlasciwosci
             save();
         }
     }
 
+    /**
+     * Przyjmuje klucz i wartosc, ktore zostaną wpisane do instancji klasy
+     * properties
+     * @param key klucz typu string
+     * @param value wartośc typu string
+     */
     public void add(String key, String value){
+        //tutaj jest wpisanie tych wartości
         properties.setProperty(key, value);
     }
 
+    /**
+     * analogicznie do mapy, properties trzymajac zestawy danych
+     * udostępnia je na podstawie klucza podanego w metodzie add(klucz, wartosc)
+     * Zatem podając klucz otrzymamy wartość zapisaną wcześniej
+     * @param key
+     * @return
+     */
     public String get(String key){
+        //odczyt
         return  properties.getProperty(key);
     }
 
+    /**
+     * zapisujemy do pliku gotowy zestaw danych (cały obiekt properties) używając
+     * mechanizmów z tej klasy
+     */
     public void save(){
         try {
+            //uzywam metody store, ponieważ dba o spójność danych w pliku
+            //podaję tylko strumien wejściowy i nazwę pliku
             properties.store(new FileOutputStream(FILENAME),null);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * Odczyt danych z pliku
+     */
     public void load(){
         try {
+            //odczyt z pliku. Podaję tylko strumien.
             properties.load(new FileInputStream(FILENAME));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * Singleton. Sprawdzam czy istnieje już instancja tej klasy
+     * i jeżeli nie, to ją tworzę, w przeciwynym wypadku zwracam
+     * juz utworzoną wcześniej.
+     * @return instancja klasy Register.
+     */
     public static Register getInstance() {
         if (instance == null) {
             instance = new Register();
